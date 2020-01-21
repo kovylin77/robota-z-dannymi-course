f1b7e3c8c37c5e29dc4a036ad465e17489342001
Mikhail Kovylin <mikhail.kovylin@dataart.com>
2020-01-21 16:55:02 +0200

    - Added gitignore

--- robota-z-dannymi/Program.cs
+++ robota-z-dannymi/Program.cs
@@ -1,9 +1,9 @@
 ﻿namespace robota_z_dannymi
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+        }
     }
-  }
 }
